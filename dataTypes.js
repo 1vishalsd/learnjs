862546c77e2c8809522327f0e74c31c6d8b6ba32
1VishalSD <ilearnvk@gmail.com>
2024-09-10 00:35:35 +0530

    commplete data types of js

--- dataTypes.js
+++ dataTypes.js
@@ -3,3 +3,21 @@
 //alert(3 + 3); // we are using nodejs, not browers
 
 console.log(3 + 3);
+// code readbility that should be high
+
+let name = " Raju singh";
+let age = 23;
+let isLoggedIn = false;
+//primitive data types
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true / false
+// null  => stand alone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undifined
+console.log(typeof null); // object
