82d1014a9d348e978c45c9fb0d278b8d92861d1c
1VishalSD <ilearnvk@gmail.com>
2024-09-17 12:53:21 +0530

    make grade system

--- day-3.js
+++ day-3.js
@@ -36,3 +36,58 @@ if (num1 >= num2) {
     console.log(`${num3} is largest number`);
   }
 }
+//  Task 3
+function getDayOfWeek(dayNumber) {
+  switch (dayNumber) {
+    case 1:
+      console.log("Sunday");
+      break;
+      case2: console.log("Monday");
+      break;
+    case 3:
+      console.log("Tuesday");
+      break;
+    case 4:
+      console.log("Wednesday");
+      break;
+    case 5:
+      console.log("Thursday");
+      break;
+    case 6:
+      console.log("Friday");
+      break;
+    case 7:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Invalid input. Please enter a number between 1 and 7.");
+  }
+}
+getDayOfWeek(6);
+
+function gradeScore(score) {
+  switch (true) {
+    case score >= 85:
+      console.log(` You got the Grade A , You scored ${score} number`);
+      break;
+    case score >= 75:
+      console.log(`You got the Grade B , You scored ${score} number`);
+      break;
+    case score >= 65:
+      console.log(`You got the Grade C , You scored ${score} number`);
+      break;
+    case score >= 55:
+      console.log(`You got the Grade D , You scored ${score} number`);
+      break;
+    case score >= 45:
+      console.log(`You got the Grade E , You scored ${score} number`);
+      break;
+    default:
+      if (score < 45 && score >= 0) {
+        console.log(` You Got grade F , You scored ${score} number`);
+      } else {
+        console.log("Invalide score");
+      }
+  }
+}
+gradeScore(50);
