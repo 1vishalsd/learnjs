57a2a36eec4ec65116db356e9614163910fb0caf
1VishalSD <ilearnvk@gmail.com>
2024-09-17 12:22:23 +0530

    find largest number in three varbiale

--- day-3.js
+++ day-3.js
@@ -16,3 +16,23 @@ const check =
     : console.log(" You are not eligible to giving the vote");
 
 // Task 2
+
+var num1 = 45;
+
+var num2 = 93;
+
+var num3 = 100;
+
+if (num1 >= num2) {
+  if (num1 >= num3) {
+    console.log(`${num1} is lagest number`);
+  } else {
+    console.log(`${num3} is largest number`);
+  }
+} else {
+  if (num2 >= num3) {
+    console.log(`${num2} is largest number`);
+  } else {
+    console.log(`${num3} is largest number`);
+  }
+}
