495fce5717358a7b361f44eb626d4426d2fea027
1VishalSD <ilearnvk@gmail.com>
2024-09-17 10:15:01 +0530

    started day-3

---
+++ day-3.js
@@ -0,0 +1,18 @@
+// Task 1
+const num = 7;
+
+if (num > 0) {
+  console.log(" Your Number is postive");
+} else if (num == 0) {
+  console.log(" Your Number is equal of zero");
+} else {
+  console.log(" Your number is negative ");
+}
+
+const age = 20;
+const check =
+  age >= 18
+    ? console.log(" You are eligible for giving the vote")
+    : console.log(" You are not eligible to giving the vote");
+
+// Task 2
