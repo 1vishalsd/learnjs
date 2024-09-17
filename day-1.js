d66c6d49ac90d8beb8ef0c8773acc22ab3a00ca6
1VishalSD <ilearnvk@gmail.com>
2024-09-17 09:34:09 +0530

    day -1 code challenge completed

---
+++ day-1.js
@@ -0,0 +1,22 @@
+// Variable Declarations
+
+var num = 34;
+
+let name = " raju";
+console.log(name);
+name = " shyam";
+
+const isLogged = false;
+isLogged = true;
+
+const raju = ["students", 20, true];
+
+const cars = {
+  brand: " BMW",
+  props: {
+    color: "Red",
+    masSpeed: " 280kmp",
+  },
+};
+console.log(num, name, isLogged, raju, cars);
+console.log(typeof num, typeof name, typeof isLogged, typeof raju, typeof cars);
---
+++ string.js
@@ -0,0 +1,8 @@
+const name = " Vishal ";
+const repoCount = 67;
+
+const gameName = new String("Vishalsd");
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// Number math --------------->
