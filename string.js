e50326a4ba94aacf42f07d0ebd3eeac3c00a6cc8
1VishalSD <ilearnvk@gmail.com>
2024-09-18 15:35:03 +0530

    delete extra file

--- string.js
+++
@@ -1,8 +0,0 @@
-const name = " Vishal ";
-const repoCount = 67;
-
-const gameName = new String("Vishalsd");
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-
-// Number math --------------->
