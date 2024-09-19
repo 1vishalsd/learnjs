f088d701b70ac4e2ef383d6bda56e0770707423f
1VishalSD <ilearnvk@gmail.com>
2024-09-19 10:21:47 +0530

    Activitices 3 is done

--- day-6.js
+++ day-6.js
@@ -23,3 +23,20 @@ console.log(arr);
 // Task 6: Use the unshift method to add a new number to the beginning of the array and log the updated array.
 arr.unshift(1, 6);
 console.log(arr);
+
+// Activity 3: Array Methods (Intermediate)
+
+// Task 7: Use the map method to create a new array where each number is doubled and log the new array.
+
+const mapArr = arr.map((x) => x * 2);
+console.log(mapArr);
+
+// Task 8: Use the filter method to create a new array with only even numbers and log the new array.
+
+const filterArr = arr.filter((x) => x > 5);
+console.log(filterArr);
+
+// Task 9: Use the reduce method to calculate the sum of all numbers in the array and log the result.
+const intialValue = 0;
+const reduceArr = arr.reduce((x, y) => x + y, intialValue);
+console.log(reduceArr);
