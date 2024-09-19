0f53e08006a4ba8c642c57c5dc30b251276015c2
1VishalSD <ilearnvk@gmail.com>
2024-09-19 16:09:56 +0530

    start to solving object part

---
+++ day-7.js
@@ -0,0 +1,13 @@
+// Activity 1: Object Creation and Access
+// Task 1: Create a book object with properties like title, author, and year. Log it to the console.
+
+const books = {
+  title: "Good man",
+  auther: " Hamit ",
+  year: 2006,
+};
+console.log(books);
+
+// Task 2: Access and log the title and author properties.
+
+console.log(books.auther, books.title);
